cf28a7740f3b3e9451cf045fc291b4ad8421e04d
359miharu <m.fujita@newdaysys.co.jp>
2021-09-22 14:03:15 +0900

    2章修正

--- lama-ex2-123.pl
+++ lama-ex2-123.pl
@@ -1,11 +1,14 @@
 #!/usr/bin/env perl
+use strict;
+use warnings;
+
 print "円の半径を入力してください。";
-chomp ($r = <STDIN>);
+chomp($r = <STDIN>);
 $p = 3.141592654;
 
-if($r ge 0){ 
-print "円周の長さは",2 * $r * $p,"です。\n";
-}else{
-print "０より小さい数が入力されました\n";
-print "円周の長さは０です。\n";
+if ($r ge 0) { 
+    print "円周の長さは",2 * $r * $p,"です。\n";
+} else {
+    print "０より小さい数が入力されました\n";
+    print "円周の長さは０です。\n";
 }
