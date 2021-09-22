47c610718dd17a96395cd59a1fe9deb1f0fe22bc
359miharu <m.fujita@newdaysys.co.jp>
2021-09-22 14:17:13 +0900

    2章修正

--- lama-ex2-123.pl
+++ lama-ex2-123.pl
@@ -3,10 +3,12 @@ use strict;
 use warnings;
 
 print "円の半径を入力してください。";
+my $r;
 chomp($r = <STDIN>);
-$p = 3.141592654;
+my $p = 3.141592654;
 
-if ($r ge 0) { 
+if ($r ge 0) {
+ 
     print "円周の長さは",2 * $r * $p,"です。\n";
 } else {
     print "０より小さい数が入力されました\n";
